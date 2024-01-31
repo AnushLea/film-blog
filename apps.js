a9862a31480b90b9fb8f4cc68fe0731e5317a252
AnushLea <anushleab@gmail.com>
2024-01-30 19:59:37 -0800

    form updates

--- apps.js
+++ apps.js
@@ -15,3 +15,13 @@ $(document).ready(function () {
   );
 });
 
+function submitForm() {
+
+  var name = document.getElementById("name").value;
+  var email = document.getElementById("email").value;
+  var message = document.getElementById("message").value;
+  var description = document.getElementById("description").value;
+
+ 
+  alert("Name: " + name + "\nEmail: " + email + "\nMessage: " + message + "\nDescription: " + description);
+}
